4a4b25df53d6bc979848f6d5f2d9916385a879ec
oliverstaton <oliver.william.staton@gmail.com>
2014-08-18 22:23:39 -0400

    Initial Commit

---
+++ Candy Crush/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Candy Crush
+//
+//  Created by Oliver Staton on 8/18/14.
+//  Copyright (c) 2014 OllieStat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Candy Crush/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Candy Crush
+//
+//  Created by Oliver Staton on 8/18/14.
+//  Copyright (c) 2014 OllieStat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
